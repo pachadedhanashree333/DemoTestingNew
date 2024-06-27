6a101955f7a39d1e781617c41d44900078cb5fce
Dhanshri-Pachade <pachadedhanashree333@gmail.com>
2024-06-27 22:57:18 +0530

    First Commit

---
+++ src/main/java/App/Calculator.java
@@ -0,0 +1,21 @@
+package App;
+
+public class Calculator {
+
+	
+		
+		public int add(int i, int j)
+		{
+			return i+j;
+		}
+		public int multiply(int i, int j)
+		{
+			return i*j;
+		}
+		public int divide(int i, int j)
+		{
+			return i/j;
+		}
+	}
+
+
